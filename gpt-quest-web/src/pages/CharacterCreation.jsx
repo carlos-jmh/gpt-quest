4ec0ce7a5f779a4b0a0a791ca5038dacee6c9a24
KTujjar <khaled@tujjar.com>
2023-10-08 03:07:25 -0400

    Added tooltips

--- gpt-quest-web/src/pages/CharacterCreation.jsx
+++ gpt-quest-web/src/pages/CharacterCreation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 
 import {
     Gi3DHammer,
@@ -9,14 +9,19 @@ import {
     GiWizardFace,
 } from "react-icons/gi"; 
 
-import {Button, ButtonGroup, Card, CardBody, Typography} from "@material-tailwind/react";
+import {Button, ButtonGroup, Card, CardBody, Typography, Tooltip} from "@material-tailwind/react";
 
 function CharacterCreation() {
+    
     const [hoveredButton, setHoveredButton] = useState(null);
-
-    const handleButtonHover = (color) => {
+    const [text, setText] = useState(null);
+    
+    const handleButtonHover = (color, className) => {
         setHoveredButton(color);
+        setText(className);
     };
+
+    
 
     return (
         <Card className="w-full max-w-screen-md p-4 shadow-none" style={{ backgroundColor: "white" }}>
@@ -24,83 +29,142 @@ function CharacterCreation() {
                 Choose your Class
             </Typography>
             <ButtonGroup className="gap-3.5 justify-center mb-4">
-                <Button
-                    className="rounded-lg"
-                    onMouseEnter={() => handleButtonHover('red')}
-                    onMouseLeave={() => handleButtonHover(null)}
-                    style={{
-                        backgroundColor: hoveredButton === 'red' ? 'red' : '',
-                        transition: 'background-color 0.3s',
+                <Tooltip content="Barbarian" 
+                    animate={{
+                        mount:{scale: 1, y: 0}, 
+                        unmount: {scale:0, y:25},
                     }}
+                    className="text-classes-barbarian"
                 >
-                    <Gi3DHammer color={"white"} style={{ fontSize: '2rem' }} />
-                </Button>
-                <Button
-                    className="rounded-lg"
-                    onMouseEnter={() => handleButtonHover('orange')}
-                    onMouseLeave={() => handleButtonHover(null)}
-                    style={{
-                        backgroundColor: hoveredButton === 'orange' ? 'orange' : '',
-                        transition: 'background-color 0.3s',
+                    <Button
+                        className="rounded-lg "
+                        onMouseEnter={() => handleButtonHover('red', 'barbarian')}
+                        onMouseLeave={() => handleButtonHover(null, null)}
+                        style={{
+                            backgroundColor: hoveredButton === 'red' ? 'red' : '',
+                            transition: 'background-color 0.3s',
+                        }}
+                        data-tooltip-target="tooltip-light"
+                        type="button"
+                    >
+                        <Gi3DHammer  color={"white"} style={{ fontSize: '2rem' }} />
+                    </Button>
+                </Tooltip>
+                <Tooltip content="Ranger" 
+                    animate={{
+                        mount:{scale: 1, y: 0}, 
+                        unmount: {scale:0, y:25},
                     }}
+                    className="text-classes-ranger"
                 >
-                    <GiBowArrow color={"white"} style={{ fontSize: '2rem' }} />
-                </Button>
-                <Button
-                    className="rounded-lg"
-                    onMouseEnter={() => handleButtonHover('green')}
-                    onMouseLeave={() => handleButtonHover(null)}
-                    style={{
-                        backgroundColor: hoveredButton === 'green' ? 'green' : '',
-                        transition: 'background-color 0.3s',
+                    <Button
+                        className="rounded-lg"
+                        onMouseEnter={() => handleButtonHover('orange')}
+                        onMouseLeave={() => handleButtonHover(null)}
+                        style={{
+                            backgroundColor: hoveredButton === 'orange' ? 'orange' : '',
+                            transition: 'background-color 0.3s',
+                        }}
+                    >
+                        <GiBowArrow color={"white"} style={{ fontSize: '2rem' }} />
+                    </Button>
+                </Tooltip>
+                <Tooltip content="Druid" 
+                    animate={{
+                        mount:{scale: 1, y: 0}, 
+                        unmount: {scale:0, y:25},
                     }}
+                    className="text-classes-druid"
                 >
-                    <GiWizardFace color={"white"} style={{ fontSize: '2rem' }} />
-                </Button>
-                <Button
-                    className="rounded-lg"
-                    onMouseEnter={() => handleButtonHover('yellow')}
-                    onMouseLeave={() => handleButtonHover(null)}
-                    style={{
-                        backgroundColor: hoveredButton === 'yellow' ? 'yellow' : '',
-                        transition: 'background-color 0.3s',
+                    <Button
+                        className="rounded-lg"
+                        onMouseEnter={() => handleButtonHover('green')}
+                        onMouseLeave={() => handleButtonHover(null)}
+                        style={{
+                            backgroundColor: hoveredButton === 'green' ? 'green' : '',
+                            transition: 'background-color 0.3s',
+                            position: 'relative',
+                        }}
+                    >
+                        <GiWizardFace color={"white"} style={{ fontSize: '2rem' }} />
+                    </Button>
+                </Tooltip>
+                <Tooltip content="Cleric" 
+                    animate={{
+                        mount:{scale: 1, y: 0}, 
+                        unmount: {scale:0, y:25},
                     }}
+                    className="text-classes-cleric"
                 >
-                    <GiHealthIncrease color={"white"} style={{ fontSize: '2rem' }} />
-                </Button>
-                <Button
-                    className="rounded-lg"
-                    onMouseEnter={() => handleButtonHover('blue')}
-                    onMouseLeave={() => handleButtonHover(null)}
-                    style={{
-                        backgroundColor: hoveredButton === 'blue' ? 'blue' : '',
-                        transition: 'background-color 0.3s',
+                    <Button
+                        className="rounded-lg"
+                        onMouseEnter={() => handleButtonHover("#ffeb3b")}
+                        onMouseLeave={() => handleButtonHover(null)}
+                        style={{
+                            backgroundColor: hoveredButton === "#ffeb3b" ? "#ffeb3b" : '',
+                            transition: 'background-color 0.3s',
+                        }}
+                    >
+                        <GiHealthIncrease color={"white"} style={{ fontSize: '2rem' }} />
+                    </Button>
+                </Tooltip>
+                <Tooltip content="Rogue" 
+                    animate={{
+                        mount:{scale: 1, y: 0}, 
+                        unmount: {scale:0, y:25},
                     }}
+                    className="text-classes-rogue"
                 >
-                    <GiRogue color={"white"} style={{ fontSize: '2rem' }} />
-                </Button>
-                <Button
-                    className="rounded-lg"
-                    onMouseEnter={() => handleButtonHover('purple')}
-                    onMouseLeave={() => handleButtonHover(null)}
-                    style={{
-                        backgroundColor: hoveredButton === 'purple' ? 'purple' : '',
-                        transition: 'background-color 0.3s',
+                    <Button
+                        className="rounded-lg"
+                        onMouseEnter={() => handleButtonHover('blue')}
+                        onMouseLeave={() => handleButtonHover(null)}
+                        style={{
+                            backgroundColor: hoveredButton === 'blue' ? 'blue' : '',
+                            transition: 'background-color 0.3s',
+                        }}
+                    >
+                        <GiRogue color={"white"} style={{ fontSize: '2rem' }} />
+                    </Button>
+                </Tooltip>
+                <Tooltip content="Warlock" 
+                    animate={{
+                        mount:{scale: 1, y: 0}, 
+                        unmount: {scale:0, y:25},
                     }}
+                    className="text-classes-warlock"
                 >
-                    <GiCursedStar color={"white"} style={{ fontSize: '2rem' }} />
-                </Button>
-                <Button
-                    className="rounded-lg"
-                    onMouseEnter={() => handleButtonHover('pink')}
-                    onMouseLeave={() => handleButtonHover(null)}
-                    style={{
-                        backgroundColor: hoveredButton === 'pink' ? 'pink' : '',
-                        transition: 'background-color 0.3s',
+                    <Button
+                        className="rounded-lg"
+                        onMouseEnter={() => handleButtonHover('purple')}
+                        onMouseLeave={() => handleButtonHover(null)}
+                        style={{
+                            backgroundColor: hoveredButton === 'purple' ? 'purple' : '',
+                            transition: 'background-color 0.3s',
+                        }}
+                    >
+                        <GiCursedStar color={"white"} style={{ fontSize: '2rem' }} />
+                    </Button>
+                </Tooltip>
+                <Tooltip content="Bard" 
+                    animate={{
+                        mount:{scale: 1, y: 0}, 
+                        unmount: {scale:0, y:25},
                     }}
+                    className="text-classes-bard "
                 >
-                    <GiBowArrow color={"white"} style={{ fontSize: '2rem' }} />
-                </Button>
+                    <Button
+                        className="rounded-lg"
+                        onMouseEnter={() => handleButtonHover('pink')}
+                        onMouseLeave={() => handleButtonHover(null)}
+                        style={{
+                            backgroundColor: hoveredButton === 'pink' ? 'pink' : '',
+                            transition: 'background-color 0.3s',
+                        }}
+                    >
+                        <GiGuitar color={"white"} style={{ fontSize: '2rem' }} />
+                    </Button>
+                </Tooltip>
             </ButtonGroup>
             <CardBody />
         </Card>
--- gpt-quest-web/tailwind.config.js
+++ gpt-quest-web/tailwind.config.js
@@ -4,11 +4,32 @@ import withMT from "@material-tailwind/react/utils/withMT";
 export default withMT({
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'wood_pattern': "url('gpt-quest-web/assets/wood-theme.jpeg')"
+      },
+      colors: {
+        classes: {
+          'barbarian': "#ff0000",
+          'ranger': "#ffa500",
+          'druid': "#008000",
+          'cleric': "#ffeb3b",
+          'rogue': "#1976d2",
+          'warlock': "#800080",
+          'bard': "#ffc0cb",
+          'wood': "#704F32",
+
+        },
+      },
+    },
   },
-  plugins: [],
-})
+
+  plugins: [
+    require('flowbite/plugin')
+  ],
+});
 
